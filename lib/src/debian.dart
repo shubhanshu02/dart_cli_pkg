614d8efbc1a26c32d24f6803057c890ea43f0ad5
Shubhanshu Saxena <shubhanshu.e01@gmail.com>
2022-01-17 16:58:52 +0530

    lib/src/debian.dart: Update repository with New Debian Packages
    This commit adds functions to update the [debianRepo] locally with
    the newly added .deb packages and sign the Release files.
    
    Signed-off-by: Shubhanshu Saxena <shubhanshu.e01@gmail.com>

--- lib/src/debian.dart
+++ lib/src/debian.dart
@@ -59,7 +59,8 @@ Future<void> _update() async {
       await cloneOrPull(url("https://github.com/$debianRepo.git").toString());
 
   await _createDebianPackage(repo, packageName);
-  // TODO: Functions to Release and Upload the package to Git Upstream
+  await _releaseNewPackage(repo);
+  // TODO: Function to Upload the package to the Git repository.
 }
 
 /// Creates a Debian package from the source code.
@@ -79,6 +80,15 @@ Future<void> _removeDirectory(String directory) async {
   if (result.exitCode != 0) {
     fail('Unable to remove the directory\n${result.stderr}');
   }
+}
+
+/// Scans the PPA [repo] for new packages and updates the
+/// release files, also signing them.
+Future<void> _releaseNewPackage(String repo) async {
+  await _updatePackagesFile(repo);
+  await _updateReleaseFile(repo);
+  await _updateReleaseGPGFile(repo);
+  await _updateInReleaseFile(repo);
 }
 
 /// Create the directory `repo/packageName` and relevant subfolders for the
@@ -113,4 +123,52 @@ void _generateExecutableFiles(String debianDir) {
 void _generateControlFile(String debianDir) {
   var controlFilePath = p.join(debianDir, "DEBIAN", "control");
   writeString(controlFilePath, controlData.value);
+}
+
+/// Scan for new .deb packages in the [repo] and update the `Packages` file.
+Future<void> _updatePackagesFile(String repo) async {
+  // Scan for new packages
+  String output = run("dpkg-scanpackages",
+      arguments: ["--multiversion", "."], workingDirectory: repo);
+  // Write the stdout to the file
+  writeString(p.join(repo, 'Packages'), output);
+  // Force Compress the Packages file
+  run("gzip", arguments: ["-k", "-f", "Packages"], workingDirectory: repo);
+}
+
+/// Generate the Release index for the PPA.
+Future<void> _updateReleaseFile(String repo) async {
+  String output = run("apt-ftparchive",
+      arguments: ["release", "."], workingDirectory: repo);
+  writeString(p.join(repo, 'Release'), output);
+}
+
+/// Sign the Release file with the GPG key.
+Future<void> _updateReleaseGPGFile(String repo) async {
+  String output = run("gpg",
+      arguments: [
+        "--default-key",
+        signingEmail.value,
+        "-abs",
+        "-o",
+        "-",
+        "Release",
+      ],
+      workingDirectory: repo);
+  writeString(p.join(repo, 'Release.gpg'), output);
+}
+
+/// Update the InRelease file with the new index and keys.
+Future<void> _updateInReleaseFile(String repo) async {
+  String output = run("gpg",
+      arguments: [
+        "--default-key",
+        signingEmail.value,
+        "--clearsign",
+        "-o",
+        "-",
+        "Release",
+      ],
+      workingDirectory: repo);
+  writeString(p.join(repo, 'InRelease'), output);
 }
